b719ef9a7b99fbf32d21310097f6000708420bd3
Melissa Moreira <xmelissamoreirax@gmail.com>
2018-09-20 20:33:41 -0300

    Aulas #83-93 | Adding Vuex + First Vuex module

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import store from './store';
 
+// Importing the vuex store in our Vue object
 
 new Vue({
+  // store: store,
+  store,
   render: h => h(App)
 }).$mount('#app');
---
+++ src/store/modules/auth.js
@@ -0,0 +1,27 @@
+const state = {
+    token: null
+};
+
+const getters = {
+
+    //state here is a param, not the const above
+
+    isLoggedIn: (state) => !!state.token
+                //returns a boolean value
+};
+
+const mutations = {
+    setToken: (state, token) => {
+        state.token = token;
+    }
+};
+
+const actions = {
+    //we use the commit function to call a mutation!
+    
+    logout: ({ commit }) => {
+        //in commit(), we pass the mutation name as first param, an the value in the second param
+
+        commit('setToken', null);
+    }
+};
