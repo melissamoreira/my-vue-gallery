9e21988311c3a99c2305701b5c66ee53b1143fa9
Melissa Moreira <xmelissamoreirax@gmail.com>
2018-12-12 21:16:33 -0200

    Aulas #99-106 | AuthHandler + Vue  Router + Lifecycle Methods + MapGetters

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -4,19 +4,22 @@
             Image Storage
         </a>
 
+        {{ isLoggedIn }}
+
         <div class="right menu">
             <a href="#" class="ui item" @click="login">
                 Login
             </a>
-        </div>        
+        </div>
     </div>
 
 </template>
 <script>
-import { mapActions } from 'vuex';
+import { mapActions, mapGetters } from 'vuex';
 
 export default {
     name: 'AppHeader',
+    computed: mapGetters(['isLoggedIn']),
     methods: mapActions(['login'])
         //Conecting an action to a component
 };
