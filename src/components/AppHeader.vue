dc193976bb258b8bd2fee4982cb74ec08ceefaa2
Melissa Moreira <xmelissamoreirax@gmail.com>
2019-02-10 17:50:02 -0200

    Aula #115 | Navigation with Router-Link

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,15 +1,15 @@
 <template>
     <div class="ui secondary pointing menu">
-        <a href="/" class="active item">
+        <router-link to="/" class="active item">
             Image Storage
-        </a>
+        </router-link>
 
         <div class="right menu">
 
             <div v-if="isLoggedIn"
                  class="horizontal_links">
-               <a class="item">Galleries</a>
-               <a class="item">Upload</a>
+               <router-link to="/" class="item">Galleries</router-link>
+               <router-link to="/upload" class="item">Upload</router-link>
                <a class="item" @click="logout">Logout</a>
             </div>
 
